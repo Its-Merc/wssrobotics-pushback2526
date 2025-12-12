9e22680dd4838e736a72d57acabf5392d48fb2d7
Daniel <danielthewang@gmail.com>
2025-12-11 16:44:12 -0800

    ctrler and drivetrain stuff

--- src/main.py
+++ src/main.py
@@ -23,12 +23,173 @@ from vex import (
     LEFT,
     DEGREES,
     AiVisionObject,
+    FORWARD,
+    REVERSE,
+    Inertial,
+    SmartDrive,
+    GearSetting,
+    Controller,
+    Thread,
 )
 
 # from typing import List, Dict, Any
 import urandom
-
 import math
+
+# constants
+IDLE_TURN_SPEED = 8
+IDLE_TURN_DIR = RIGHT
+
+GRAB_SIZE_REQ = 35
+GRAB_X_RANGE = 35
+GRAB_Y_POS = 145
+
+CTRLER_DEADZONE = 5
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+br = Brain()
+scr = br.screen  # 480x240 px screen
+
+# setup drivetrain
+# from the auto generator
+left_drive_smart = Motor(0, GearSetting.RATIO_18_1, False)
+right_drive_smart = Motor(1, GearSetting.RATIO_18_1, True)
+inertial_sensor = Inertial(2)
+drivetrain = SmartDrive(
+    left_drive_smart, right_drive_smart, inertial_sensor, 319.19, 320, 40, MM, 1
+)
+
+# controller
+ctrler = Controller(PRIMARY)
+ctrler_l_dead = False
+ctrler_r_dead = False
+
+
+# Make random actually random
+# from the auto generator
+def initialize_random_seed():
+    wait(100, MSEC)
+    random = (
+        br.battery.voltage(MV)
+        + br.battery.current(CurrentUnits.AMP) * 100
+        + br.timer.system_high_res()
+    )
+    urandom.seed(int(random))
+
+
+# from the auto generator
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+
+drivetrain_calibrated = False
+
+
+# from the auto generator
+def calibrate_drivetrain():
+    # global drivetrain_is_calibrated, scr, inertial_sensor
+    # Calibrate the Drivetrain Inertial
+    global drivetrain_calibrated
+    sleep(200, MSEC)
+    scr.print("Calibrating")
+    scr.next_row()
+    scr.print("Inertial")
+    inertial_sensor.calibrate()
+    while inertial_sensor.is_calibrating():
+        sleep(25, MSEC)
+    drivetrain_calibrated = True
+    scr.clear_screen()
+    scr.set_cursor(1, 1)
+
+
+# from the auto generator
+# define a task that will handle monitoring inputs from ctrler
+def ctrler_loop():
+    global ctrler_l_dead, ctrler_r_dead, remote_control_code_enabled
+    # process the controller input every 20 milliseconds
+    # update the motors based on the input values
+    while True:
+        if remote_control_code_enabled:
+            # stop the motors if the brain is calibrating
+            if inertial_sensor.is_calibrating():
+                left_drive_smart.stop()
+                right_drive_smart.stop()
+                while inertial_sensor.is_calibrating():
+                    sleep(25, MSEC)
+
+            # calculate the drivetrain motor velocities from the controller joystick axies
+            # left = axis3
+            # right = axis2
+            drivetrain_left_side_speed = ctrler.axis3.position()
+            drivetrain_right_side_speed = ctrler.axis2.position()
+
+            # check if the value is inside of the deadband range
+            if (
+                drivetrain_left_side_speed < CTRLER_DEADZONE
+                and drivetrain_left_side_speed > -CTRLER_DEADZONE
+            ):
+                # check if the left motor has already been stopped
+                if ctrler_l_dead:
+                    # stop the left drive motor
+                    left_drive_smart.stop()
+                    # tell the code that the left motor has been stopped
+                    ctrler_l_dead = True
+            else:
+                # reset the toggle so that the deadband code knows to stop the left motor next
+                # time the input is in the deadband range
+                ctrler_l_dead = False
+            # check if the value is inside of the deadband range
+            if (
+                drivetrain_right_side_speed < CTRLER_DEADZONE
+                and drivetrain_right_side_speed > -CTRLER_DEADZONE
+            ):
+                # check if the right motor has already been stopped
+                if ctrler_r_dead:
+                    # stop the right drive motor
+                    right_drive_smart.stop()
+                    # tell the code that the right motor has been stopped
+                    ctrler_r_dead = True
+            else:
+                # reset the toggle so that the deadband code knows to stop the right motor next
+                # time the input is in the deadband range
+                ctrler_r_dead = False
+
+            # only tell the left drive motor to spin if the values are not in the deadband range
+            if not ctrler_l_dead:
+                left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
+                left_drive_smart.spin(FORWARD)
+            # only tell the right drive motor to spin if the values are not in the deadband range
+            if not ctrler_r_dead:
+                right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
+                right_drive_smart.spin(FORWARD)
+        # wait before repeating the process
+        wait(20, MSEC)
+
+
+# define variable for remote controller enable/disable
+remote_control_code_enabled = True
+
+rc_auto_loop_thread_controller_1 = Thread(ctrler_loop)
+
+# vision sensor
+vision = AiVision(3, AiVision.ALL_AIOBJS)
+
+# other stuff
+intake = Motor(4)
+
+red = 0x00FF0000
+blue = 0x000000FF
+
+
+class Side:
+    def __init__(self, color: int):
+        self.color = BLUE.name if color == BLUE.as_int else RED.name
 
 
 class EnumObj:
@@ -40,48 +201,18 @@ class EnumObj:
 BLUE = EnumObj("BLUE", 0)
 RED = EnumObj("RED", 1)
 
-
-class Side:
-    def __init__(self, color: int):
-        self.color = BLUE.name if color == BLUE.as_int else RED.name
-
-
-# wait for rotation sensor to fully initialize
-wait(30, MSEC)
-
-
-br = Brain()
-scr = br.screen  # 480x240 px screen
-
-
-# Make random actually random
-def initializeRandomSeed():
-    wait(100, MSEC)
-    random = (
-        br.battery.voltage(MV)
-        + br.battery.current(CurrentUnits.AMP) * 100
-        + br.timer.system_high_res()
-    )
-    urandom.seed(int(random))
-
-
-def play_vexcode_sound(sound_name):
-    # Helper to make playing sounds from the V5 in VEXcode easier and
-    # keeps the code cleaner by making it clear what is happening
-    print("VEXPlaySound:" + sound_name)
-    wait(5, MSEC)
+# 0 = blue, 1 = red
+TEAM = RED
 
 
 # takes a snapshot with the vision sensor and picks the first block
 # then it uses the block's x position and trigonometry to find how many degrees to turn the robot toward the block
-def align_to_block(block_index: int):
+def align_to_block(block_xpos: int):
     global drivetrain
-    objs = vision.take_snapshot(AiVision.ALL_AIOBJS)
-    obj = objs[block_index]
 
     # 320x240 screen
     # relative position to the center
-    scr_x = obj.centerX - 160
+    scr_x = block_xpos - 160
 
     # find the angle with inverse tangent
     angle_to_turn = math.degrees(math.atan2(1, scr_x))
@@ -91,7 +222,7 @@ def align_to_block(block_index: int):
     drivetrain.turn_for(direction, abs(angle_to_turn), DEGREES)
 
 
-def grabbable_blocks(
+def detecting_grabbables(
     objs: list, grabbable_size: int, x_range: int = 35, min_y: int = 145
 ) -> bool:
     # if any obj in the detected objs is grabbable, return true
@@ -135,23 +266,9 @@ def on_usr_control():
 # create competition instance
 comp = Competition(on_usr_control, on_auto)
 
-# setup drivetrain
-m1 = Motor(1)
-m2 = Motor(2)
-m3 = Motor(3)
-m4 = Motor(4)
-mg_left = MotorGroup(m1, m3)
-mg_right = MotorGroup(m2, m4)
-drivetrain = DriveTrain(mg_left, mg_right)
-
-# vision sensor
-vision = AiVision(0, AiVision.ALL_AIOBJS)
-
-red = 0x00FF0000
-blue = 0x000000FF
-
-# \/ on program starts \/
-initializeRandomSeed()
+# \/ -------------------------- on program starts -------------------------- \/
+initialize_random_seed()
+calibrate_drivetrain()
 
 scr.clear_screen()
 scr.print("Starting...")
@@ -162,12 +279,42 @@ scr.draw_circle(280, 120, 25, 0x00F0000)
 scr.draw_rectangle(220, 40, 40, 80, 0x00F0000)
 
 while True:
-    # get detected objects
-    objs = vision.take_snapshot(AiVision.ALL_AIOBJS)
-
     # clear the screen
     scr.clear_screen()
 
+    # get detected objs
+    objs = vision.take_snapshot(AiVision.ALL_AIOBJS)
+
+    # get objs on own team colour
+    target_objs = [obj for obj in objs if obj.id == TEAM.as_int]
+
+    # TODO should probably update this later
+    target_obj = target_objs[0] if len(target_objs) > 0 else obj
+
+    # if anything we want to grab is detected
+    if len(target_objs) > 0:
+        # align to the first one
+        align_to_block(target_obj.centerX)
+    else:  # otherwise do something else
+        drivetrain.turn_for(IDLE_TURN_DIR, IDLE_TURN_SPEED, DEGREES)
+
+    # if we can grab something, grab it
+    if detecting_grabbables(target_objs, GRAB_SIZE_REQ, GRAB_X_RANGE, GRAB_Y_POS):
+        # spin the intake motor and stop moving
+        intake.spin(FORWARD)
+        # drivetrain.stop()
+    # if we can grab something but wrong team, dont
+    elif detecting_grabbables(objs, GRAB_SIZE_REQ, GRAB_X_RANGE, GRAB_Y_POS):
+        # TODO considering changing this
+        intake.spin(REVERSE)
+        # drivetrain.drive(REVERSE)
+    else:
+        intake.stop()
+
+    # drive
+    drivetrain.drive(FORWARD)
+
+    # debug stuff
     # enumerate does not work apparently so...
     for i in range(len(objs)):
         obj = objs[i]
@@ -175,9 +322,10 @@ while True:
         # color of the block
         color = Side(obj.id)
 
-        # debug stuff
+        # set cursor for info
         scr.set_cursor((4 * i) + 1, 1)
 
+        # info stuff
         scr.print(obj.centerX, obj.centerY, sep=", ")
         scr.next_row()
         scr.print(obj.originX, obj.originY, sep=", ")
@@ -186,6 +334,7 @@ while True:
         scr.next_row()
         scr.print(obj.score)
 
+        # draw rectangle around the object
         scr.draw_rectangle(
             obj.originX,
             obj.originY,
@@ -196,6 +345,16 @@ while True:
                 | max(0, min(255, math.ceil((1 - (obj.score / 100)) * 255))) << 24
             ),
         )
+
+        # highlight the target object
+        if obj == target_obj:
+            scr.draw_rectangle(
+                obj.centerX - 2,
+                obj.centerY - 2,
+                obj.width + 4,
+                obj.height + 4,
+                0x00FFFFFF,
+            )
 
     # vision sensor bounds and center point
     scr.draw_pixel(160, 120)
